e2f74f62aa30902029582ee6daae8be06af0707b
bmallexander <allexander.bergmans@outlook.com>
2024-08-22 09:12:24 +0200

    Base

---
+++ Editor/GameObject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Editor.GameObject; // Missing semicolon
+
+namespace Editor.GameObject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == openProjectButton)
+            {
+                if (createProjectButton.IsChecked == true)
+                {
+                    createProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+                openProjectButton.IsChecked = true;
+            }
+            else if (sender == createProjectButton) // Fixed this condition
+            {
+                if (openProjectButton.IsChecked == true)
+                {
+                    openProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800, 0, 0, 0);
+                }
+                createProjectButton.IsChecked = true; // Fixed to correctly set createProjectButton.IsChecked
+            }
+        }
+    }
+}
